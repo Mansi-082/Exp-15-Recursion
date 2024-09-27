40c9722bae0a8038e2113972080c0646b86b0195
Mansi-082 <mansi.kulkarni.btech2023@sitpune.edu.in>
2024-09-27 09:28:35 +0530

    1. Factorial

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,16 +1,20 @@
+//Mansi Kulkarni
+//2370123082
+//ENTC B1
+
 #include<iostream>
 using namespace std;
 
-int factorial(int n);
+int factorial(int n); //calling function
 int main() {
     int n;
     cout << "Enter a number: ";
     cin >> n;
-    cout << "Factorial of " << n << " = " << factorial(n);
+    cout << "Factorial of " << n << " = " << factorial(n);  //recursion
     return 0;
 }
 
-int factorial(int n) {
+int factorial(int n) { //declaring function
     if(n==0){
         return 1;
     } else{
